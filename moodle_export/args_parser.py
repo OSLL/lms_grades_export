f4e69bd377a4a5301e47f54101dba780837fa6ab
poiqwq <each@fsb.news>
2022-07-16 02:20:20 +0300

    Кастомизируемый список полей для экспорта

--- moodle_export/args_parser.py
+++ moodle_export/args_parser.py
@@ -11,5 +11,6 @@ def arg_parser():
     parser.add_argument('--table_id', type=str, required=False, help='Specify Google sheet document id (can find in url)')
     parser.add_argument('--sheet_id', type=str, required=False, help='Specify title for a sheet in a document in which data will be printed')
     parser.add_argument('--percentages', required=False, action='store_true', help='If set then grades will be printed as percentages')
+    parser.add_argument('--options', type=lambda s: [i for i in s.split(',')], required=False, help='Specify options for column names')
     args = parser.parse_args()
     return args
