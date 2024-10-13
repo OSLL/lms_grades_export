d74564be67b5e5a30c9648d158f497eb4f6bc109
Dmitry Ivanov <darcenrall@gmail.com>
2024-10-14 02:02:59 +0300

    cp yadisk_manager.py to moodle_export

---
+++ moodle_export/yadisk_manager.py
@@ -0,0 +1,44 @@
+"""Script with DiskManager class and functions for moodle backup
+
+"""
+from datetime import datetime
+from os import environ, path
+from logging import getLogger
+
+import yadisk
+
+
+logger = getLogger()
+
+
+class DiskManager():
+    """Light YaDisk manager
+    """
+
+    def __init__(self, download_path='./'):
+        self.client = yadisk.Client(token=environ.get('YADISK_TOKEN'))
+        self.download_path = download_path
+
+    def upload(self, local_path: str, disk_path: str, overwrite=True):
+        """upload from local_path to disk_path
+
+        Args:
+            local_path (str): path to local file 
+            disk_path (str): full path to file on yadisk
+            overwrite (bool): overwrite file. Defaults to true
+        """
+        logger.info("Uploading %s to %s", *(local_path, disk_path))
+        self.client.upload(local_path, disk_path, overwrite=overwrite)
+
+    def download_file_from_disk(self, remote_path: str):
+        """_summary_
+
+        Args:
+            remote_path (str): full path to file on yadisk
+
+        Returns:
+            str: path to downloaded file 
+        """
+        local_path = self.download_path + path.basename(remote_path)
+        self.client.download(remote_path, local_path)
+        return local_path
